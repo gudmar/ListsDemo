1d0f5267b891d2c9f0f57046725a8360ab8a6a12
Marek Gudalewicz <marek.gudalewicz@localhost>
2023-07-23 01:34:36 +0200

    Deletion function implemented

--- lists-demo/src/Functions/processTree.ts
+++ lists-demo/src/Functions/processTree.ts
@@ -45,8 +45,9 @@ const processGaneral = ({
     tree, conditionFunction, actionFunction
 }: iConditionFunction) => {
     if (isArray(tree)) {
-        tree.forEach((item: any)=>{processObject({tree: item, conditionFunction, actionFunction})});
+        tree.forEach((item: any)=>{ processObject({tree: item, conditionFunction, actionFunction })});
     } else if (isObject(tree)) {
+
         processObject({tree, conditionFunction, actionFunction});
     }
     return tree;
@@ -56,4 +57,70 @@ export const processTree = ({tree, conditionFunction, actionFunction}: iConditio
     throwIfNotObject(tree);
     processGaneral({tree, conditionFunction, actionFunction});
     return tree
+}
+
+interface iDeleteBranch {
+    tree: {[key: string]: any},
+    conditionFunction: ConditionFunctionType,
+}
+
+
+const tryDeletingObject = (parent: any, key: string, conditionFunction: ConditionFunctionType) => {
+    const shouldDelete = conditionFunction(parent[key])
+    console.log('Should delete ', shouldDelete)
+    if (shouldDelete) {
+        delete parent[key];
+    }
+    return parent;
+}
+
+const removeFromArrayIfMatch = (parent: any, key: string, conditionFunction: ConditionFunctionType) => {
+    if (isArray(parent[key])) {
+        const newArr = parent[key].filter((tree: any) => !conditionFunction(tree));
+        console.log(newArr);
+        parent[key] = newArr;
+    }
+}
+
+const tryDeletingChildren = (parent: any, key: string, conditionFunction: ConditionFunctionType) => {
+    if (!isObject(parent[key])) return;
+    const children = Object.keys(parent[key]);
+    children.forEach((child) => {
+        removeGeneral(parent[key], child, conditionFunction);
+        const shouldDelete = conditionFunction(parent[key][child]);
+        if (shouldDelete) {
+            delete parent.key.child
+        }
+    })
+}
+
+const removeChildrenOfNode = (parent: any, conditionFunction: ConditionFunctionType) => {
+    if (!isObject(parent)) return;
+    const childKeys = Object.keys(parent);
+    childKeys.forEach((key: string) => {
+        removeGeneral(parent, key, conditionFunction);
+    });
+}
+
+const removeChildrenOfAllArrayElements = (parent: any, key: string, conditionFunction: ConditionFunctionType) => {
+    parent[key].forEach((item: any) => {
+        removeChildrenOfNode(item, conditionFunction);
+    })
+}
+
+const removeGeneral = (parent: any, key: string, conditionFunction: ConditionFunctionType) => {
+    if (isArray(parent[key])) {
+        removeChildrenOfAllArrayElements(parent, key, conditionFunction);
+        removeFromArrayIfMatch(parent, key, conditionFunction)
+    } if (isObject(parent[key])) {
+        tryDeletingChildren(parent, key, conditionFunction)
+        tryDeletingObject(parent, key, conditionFunction)
+    }
+}
+
+export const deleteBranch = ({tree, conditionFunction}: iDeleteBranch) => {
+    throwIfNotObject(tree);
+    const parent = {root: tree};
+    removeGeneral(parent, 'root', conditionFunction);
+    return parent.root;
 }
