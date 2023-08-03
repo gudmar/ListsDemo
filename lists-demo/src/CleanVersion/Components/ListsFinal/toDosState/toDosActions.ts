8e1a2e26705d262ee7fdc06525fda0b9ba3df960
Marek Gudalewicz <marek.gudalewicz@localhost>
2023-08-04 01:33:51 +0200

    todos state handler implemented

--- lists-demo/src/CleanVersion/Components/ListsFinal/toDosState/toDosActions.ts
+++ lists-demo/src/CleanVersion/Components/ListsFinal/toDosState/toDosActions.ts
@@ -1,4 +1,4 @@
-import { tPayload } from "../../../../Types/dataTypes";
+import { ToDoData, tPayload } from "../../../../Types/dataTypes";
 
 export const EDIT_TODOS_MESSAGE = 'edit todos message';
 export const EDIT_TODOS_DONE_STAGE = 'edit todos done stage';
@@ -8,28 +8,28 @@ export const SET_TODOS_STATE = 'set todos state';
 export const DELETE_TODOS_ITEM = 'delete todos item';
 export const ADD_TODOS_ITEM = 'add todos item';
 
-export const editMessageAction = (payload: tPayload) => ({
+export const editTodosMessageAction = (payload: tPayload) => ({
     type: EDIT_TODOS_MESSAGE, payload
 })
 
-export const editDoneStageAction = (payload: tPayload) => ({
+export const editTodosDoneStageAction = (payload: tPayload) => ({
     type: EDIT_TODOS_DONE_STAGE, payload
 })
 
-export const editIsDoneAction = (payload: tPayload) => ({
+export const editTodosIsDoneAction = (payload: tPayload) => ({
     type: EDIT_TODOS_IS_DONE, payload
 })
 
-export const editNotesAction = (payload: tPayload) => ({
+export const editTodosNotesAction = (payload: tPayload) => ({
     type: EDIT_TODOS_NOTES, payload
 })
 
-export const deleteItemAction = (payload: tPayload) => ({
+export const deleteTodosItemAction = (payload: tPayload) => ({
     type: DELETE_TODOS_ITEM, payload
 })
 
-export const addItemAction = (payload: tPayload) => ({
+export const addTodosItemAction = (payload: tPayload) => ({
     type: ADD_TODOS_ITEM, payload,
 })
 
-export const setStateAction = (payload: tPayload) => ({type: SET_TODOS_STATE, payload})
+export const setTodosStateAction = (payload: ToDoData[]) => ({type: SET_TODOS_STATE, payload})
--- lists-demo/src/CleanVersion/Hooks/useNotesState.ts
+++ lists-demo/src/CleanVersion/Hooks/useNotesState.ts
@@ -1,18 +1,17 @@
 import { useEffect, useReducer } from "react"
-import { LOCAL_STORAGE_KEY } from "../../Const/const";
+import { LOCAL_STORAGE_NOTES } from "../../Const/const";
 import { NoteData } from "../../Types/dataTypes";
-import { addNotesItemAction, deleteNotesItemAction, setNotesStateAction } from "../Components/ListsFinal/notesListState/notesActions";
+import { addNotesItemAction, deleteNotesItemAction, editNotesMessageAction, setNotesStateAction } from "../Components/ListsFinal/notesListState/notesActions";
 import { getInitialNotesState, notesReducer } from "../Components/ListsFinal/notesListState/notesReducer";
-import { editNotesAction } from "../Components/ListsFinal/toDosState/toDosActions";
 import { useLoadData } from "./useLoadData";
 
 export const useNotesState = () => {
     const [state, dispatch] = useReducer(notesReducer, getInitialNotesState());
-    const {data} = useLoadData(getInitialNotesState(), LOCAL_STORAGE_KEY)
+    const {data} = useLoadData(getInitialNotesState(), LOCAL_STORAGE_NOTES)
     useEffect(() => {
         setNotesStateAction(data)
     }, [data])
-    const editNote = (notes: string, index: number) => dispatch(editNotesAction({data: notes, index}));
+    const editNote = (notes: string, index: number) => dispatch(editNotesMessageAction({data: notes, index}));
     const addNote = (initialState: NoteData, index: number) => dispatch(addNotesItemAction({data: initialState, index}))
     const deleteNote = (index: number) => dispatch(deleteNotesItemAction({index}))
     const setNotes = (newState: NoteData[]) => dispatch(setNotesStateAction(newState))
